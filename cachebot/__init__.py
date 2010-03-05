2bb3c6488e7869bb28548f6183b9bb035057d4b6
ziegler <ziegler@958ebe8d-e867-4fa8-abf3-4224f1f6087b>
2010-03-05 18:18:46 +0000

    script to patch the default django manager
    git-svn-id: https://mingles.svn.cvsdude.com/mingle/django-cachebot@1998 958ebe8d-e867-4fa8-abf3-4224f1f6087b

--- cachebot/__init__.py
+++ cachebot/__init__.py
@@ -1,4 +1,10 @@
-from django.conf import settings
+VERSION = (0,1)
+__version__ = '.'.join(map(str, VERSION))
 
-CACHE_SECONDS = getattr(settings,'CACHE_SECONDS',2591000)
-CACHE_PREFIX = getattr(settings,'CACHE_PREFIX','')
+try:
+    from django.conf import settings
+
+    CACHE_SECONDS = getattr(settings,'CACHE_SECONDS',2591000)
+    CACHE_PREFIX = getattr(settings,'CACHE_PREFIX','')
+except ImportError:
+    pass
--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 from setuptools import setup, find_packages
 import cachebot
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+README = read('README.rst')
+
 
 setup(
     name = "django-cachebot",
     version = cachebot.__version__,
     description = 'Automated caching and invalidation for the Django ORM',
+    long_description = README,
+    url = 'http://github.com/dziegler/django-cachebot',
     author = 'David Ziegler',
     author_email = 'david.ziegler@gmail.com',
     license = 'BSD',
     zip_safe = False,
     packages = find_packages(),
-    requires = [
-        'django==1.1',
+    package_data = {'cachebot':['patches/1-1-1-final-0/*.py']},
+    install_requires = [
+        'django==1.1.1',
     ],
     classifiers = [
         'Environment :: Web Environment',
