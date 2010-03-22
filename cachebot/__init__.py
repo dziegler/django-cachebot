8c0ce5ce01068b94c3f10f0e9d441e682a49c615
ziegler <ziegler@958ebe8d-e867-4fa8-abf3-4224f1f6087b>
2010-03-22 08:36:37 +0000

    convert CACHE_PREFIX from unicode
    git-svn-id: https://mingles.svn.cvsdude.com/mingle/django-cachebot@2445 958ebe8d-e867-4fa8-abf3-4224f1f6087b

--- cachebot/__init__.py
+++ cachebot/__init__.py
@@ -8,7 +8,7 @@ try:
     import django.dispatch
 
     CACHE_SECONDS = getattr(settings,'CACHE_SECONDS',2591000)
-    CACHE_PREFIX = getattr(settings,'CACHE_PREFIX','')
+    CACHE_PREFIX = getattr(settings,'CACHE_PREFIX','').encode('utf-8')
     CACHEBOT_CACHE_GET = getattr(settings,'CACHEBOT_CACHE_GET',False)
     CACHEBOT_CACHE_ALL = getattr(settings,'CACHEBOT_CACHE_ALL',False)
     CACHEBOT_TABLE_BLACKLIST = getattr(settings,'CACHEBOT_TABLE_BLACKLIST',('django_session',))
