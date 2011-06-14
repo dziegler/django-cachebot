03b622d74e667211745d1b9ab2371f3fd653cee8
dziegler <david.ziegler@gmail.com>
2011-06-14 16:05:38 -0400

    fix DummyCache import error

--- cachebot/backends/dummy.py
+++ cachebot/backends/dummy.py
@@ -7,10 +7,6 @@ from cachebot.logger import CacheLogDecorator
 @CacheLogDecorator
 class DummyCache(dummy.DummyCache):
 
-    def __init__(self, *args, **kwargs):
-        super(DummyCache, self).__init__(*args, **kwargs)
-        self._logger = CacheLogger()
-        
     def replace(self, **kwargs):
         pass
         
