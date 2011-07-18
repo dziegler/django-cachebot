4b36e0bf70e581fe5c6a4d64344455ef4ae9b6b8
dziegler <david.ziegler@gmail.com>
2011-07-18 14:57:53 -0400

    add missing append/prepend methods to dummy cache backend

--- cachebot/backends/dummy.py
+++ cachebot/backends/dummy.py
@@ -6,6 +6,12 @@ from cachebot.logger import CacheLogDecorator
 
 @CacheLogDecorator
 class DummyCache(dummy.DummyCache):
+    
+    def append(self, **kwargs):
+        pass
+    
+    def prepend(self, **kwargs):
+        pass
 
     def replace(self, **kwargs):
         pass
